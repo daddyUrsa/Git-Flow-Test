6f4b3a3abf00fb1fb4a0ba341c484507a91af483
daddyUrsa <0057747@gmail.com>
2021-05-26 18:08:21 +0300

    Initial Commit

---
+++ Shared/Git_Flow_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Flow_TestApp.swift
+//  Shared
+//
+//  Created by Alexey Pavlov on 26.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Git_Flow_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
